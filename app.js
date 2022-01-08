8d2988ba25909c2687b5793301890b0791354ce1
shinyelee <shinye0213@naver.com>
2022-01-08 15:31:35 +0900

    add ctx, painting-functions

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#1c1c1c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 function stopPainting() {
   painting = false;
 }
+function startPainting() {
+  painting = true;
+}
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
-
 function onMouseDown(event) {
-  painting = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
-}
-
-function onMouseLeave(event) {
-  stopPainting();
+  startPainting();
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", onMouseLeave);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
