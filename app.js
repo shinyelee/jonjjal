4f7d4864cc96f95aeeca9f81b368c5b0eb5ad1cc
shinyelee <shinye0213@naver.com>
2022-01-08 15:53:28 +0900

    add comments

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 700;
-canvas.height = 700;
+// 직접 값 설정 -> 캔버스 크기를 가져오기
+// canvas.height = 700;
+// canvas.width = 700;
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
 ctx.strokeStyle = "#1c1c1c";
 ctx.lineWidth = 2.5;
@@ -20,9 +23,11 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
+    // 클릭X -> 대기
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
+    // 클릭O -> 실제 선 그림
     ctx.lineTo(x, y);
     ctx.stroke();
   }
@@ -32,8 +37,9 @@ function onMouseDown(event) {
 }
 
 if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("mouseleave", stopPainting);
+  // 캔버스 기준 마우스 위치가
+  canvas.addEventListener("mousemove", onMouseMove); // 캔버스 안
+  canvas.addEventListener("mousedown", startPainting); // 클릭 중
+  canvas.addEventListener("mouseup", stopPainting); // 클릭 끝
+  canvas.addEventListener("mouseleave", stopPainting); // 캔버스 밖
 }
