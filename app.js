f15098d82834ff0bbc7d49cdc37365901085d950
shinyelee <shinye0213@naver.com>
2022-09-16 17:21:49 +0900

    arc, strokeStyle

--- app.js
+++ app.js
@@ -8,22 +8,34 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
+// arc -> 원 그릴 수 있음
 
+// 머리
+ctx.arc(400, 400, 100, Math.PI, 2 * Math.PI);
+// 왼쪽 뿔
+ctx.moveTo(320, 280);
+ctx.lineTo(400, 390);
+// 오른쪽 뿔
+ctx.moveTo(480, 280);
+ctx.lineTo(400, 390);
 // 선 굵기 설정
-ctx.lineWidth = 2;
-// 비워진 사각형(stroke+rect)
-ctx.strokeRect(300, 300, 50, 100);
-// 채워진 사각형(fill+rect)
-ctx.fillRect(200, 200, 200, 20);
-
-// 시작점 이동
-ctx.moveTo(200, 200);
-// 선 긋기 위한 좌표 찍고
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
-// 채우기
+ctx.lineWidth = 5;
+// 선 및 채우기 색 설정
+ctx.strokeStyle = "MediumSpringGreen";
+ctx.fillStyle = "MediumSpringGreen";
+// 머리 및 뿔 완성
+ctx.stroke();
 ctx.fill();
 
-// -> 집 완성
+ctx.beginPath();
+
+// 왼쪽 눈
+ctx.arc(360, 350, 7.5, 0, 2 * Math.PI);
+// 오른쪽 눈
+ctx.arc(440, 350, 7.5, 0, 2 * Math.PI);
+// 채우기 색 설정
+ctx.fillStyle = "white";
+// 눈 완성
+ctx.fill();
+
+// 안드로이드 로고 완성!
